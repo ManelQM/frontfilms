83512cc38da22711399e5ee113866fac507347e3
Manel <quemonmanel@gmail.com>
2024-04-19 11:47:13 +0200

    feat:dinamic search

--- frontfilms/src/components/Search.jsx
+++ frontfilms/src/components/Search.jsx
@@ -1,39 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export const Search = ({listState, setListState}) => {
-  const [search, setSearch] = useState(""); 
+export const Search = ({ listState, setListState }) => {
+  const [search, setSearch] = useState("");
+  const [notFound, setNotFound] = useState(false);
   const searchFilm = (e) => {
     //Crear estado y actualizarlo
-    setSearch(e.target.value); 
+    setSearch(e.target.value);
     //Listado completo de películas
     //Filtrar para buscar coincidencias
-    let findFilm = listState.filter(film => {
+    let findFilm = listState.filter((film) => {
       return film.title.toLowerCase().includes(search.toLocaleLowerCase()); //comprueba en cada uno de los casos si hay coincidencia
-    })
-     
-    if(search.length <= 1){
-      findFilm = JSON.parse(localStorage.getItem("films")); 
+    });
+    //Comprobar si hay un resultado
+    if (search.length <= 1 || findFilm <= 0) {
+      findFilm = JSON.parse(localStorage.getItem("films"));
+      setNotFound(true);
+    } else {
+      setNotFound(false);
     }
-    //Comprobar si hay un resultado 
-    
     //Actualizar estado del listado principal con lo que he logrado filtrar
-    setListState(findFilm); 
-  }
+    setListState(findFilm);
+  };
   return (
-    <> 
+    <>
       <div className="search">
-                <h3 className="title">Buscador : {search}</h3>
-                <form>
-                    <input type="text"
-                    name='search'
-                    autoComplete='off'
-                    value={search}
-                    onChange={searchFilm}/>
-                    <button>Buscar</button>
-                </form>
-            </div>
-    </>
-  )
-}
+        <h3 className="title">Buscador : {search}</h3>
+        {(notFound == true && search.length > 1) &&(
+          <span className="not-found">
+            No se ha encotrado ninguna coincidencia
+          </span>
+        )}
 
-export default Search; 
+        <form>
+          <input
+            type="text"
+            name="search"
+            autoComplete="off"
+            value={search}
+            onChange={searchFilm}
+          />
+          <button>Buscar</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Search;
